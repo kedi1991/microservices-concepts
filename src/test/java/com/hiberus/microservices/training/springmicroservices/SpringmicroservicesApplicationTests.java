ea0ebd1f7f7a56ddba11f0035910d2242e31fdf9
Okurut Henry Kedi <okurut.henry@yahoo.com>
2024-03-30 19:42:32 +0100

    first commit

---
+++ src/test/java/com/hiberus/microservices/training/springmicroservices/SpringmicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiberus.microservices.training.springmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
