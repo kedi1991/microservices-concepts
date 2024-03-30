cd8b1e153ed268c513704f9c1b1a31d386febfc0
Okurut Henry Kedi <okurut.henry@yahoo.com>
2024-03-31 00:18:47 +0100

    create a random number generator service

---
+++ src/main/java/com/hiberus/microservices/training/springmicroservices/service/RandomGeneratorService.java
@@ -0,0 +1,9 @@
+package com.hiberus.microservices.training.springmicroservices.service;
+
+/**
+ * @return a random factor between 10 and 50
+ */
+public interface RandomGeneratorService {
+
+    int generateRandomFactor();
+}
