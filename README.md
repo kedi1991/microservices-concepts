eb446dbb8c4d571ed88aa1806965d79e05a2ab48
Okurut Henry Kedi <okurut.henry@yahoo.com>
2024-03-31 01:23:31 +0100

    create a first integration test with BDD from Mockito

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Notes for the upcoming program on Udemy
 
 - Set up a spring boot project (java 21, web dependency)
 - make sure java_home is version 21
-- 
+
+@MockBean: A bean is any object managed by the spring container (@service, @component, ...)
+@Autowired: manages all dependencies and auto injects them when needed
---
+++ src/test/java/com/hiberus/microservices/training/springmicroservices/service/MultiplicationServiceTest.java
@@ -0,0 +1,40 @@
+package com.hiberus.microservices.training.springmicroservices.service;
+
+import com.hiberus.microservices.training.springmicroservices.domain.Multiplication;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.BDDAssumptions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MultiplicationServiceTest {
+
+    @MockBean
+    private RandomGeneratorService randomGeneratorService;
+
+    private MultiplicationService multiplicationService;
+
+    @Test
+    public void createRandomMultiplicationTest(){
+
+        //BDD: given our mocked service generator with return 50 and 30
+        given(randomGeneratorService.generateRandomFactor()).willReturn(50,30);
+
+        //when
+        Multiplication multiplication = multiplicationService.createRandomMultiplication();
+
+        //then
+        assertThat(multiplication.getFactorA()).isEqualTo(50);
+        assertThat(multiplication.getFactorB()).isEqualTo(30);
+        assertThat(multiplication.getResult()).isEqualTo(1500);
+    }
+
+}
