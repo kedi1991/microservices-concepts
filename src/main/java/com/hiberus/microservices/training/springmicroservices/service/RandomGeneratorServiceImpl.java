24f9b662d52f589e5b154957ce54f5319cd4a9b9
Okurut Henry Kedi <okurut.henry@yahoo.com>
2024-04-01 12:57:57 +0200

    Correct test class for the randomgenertorserviceImpl

--- src/main/java/com/hiberus/microservices/training/springmicroservices/service/RandomGeneratorServiceImpl.java
+++ src/main/java/com/hiberus/microservices/training/springmicroservices/service/RandomGeneratorServiceImpl.java
@@ -1,8 +1,13 @@
 package com.hiberus.microservices.training.springmicroservices.service;
 
+import java.util.Random;
+
 public class RandomGeneratorServiceImpl implements  RandomGeneratorService{
+
+    final static int MIN_FACTOR = 10;
+    final static int MAX_FACTOR = 20;
     @Override
     public int generateRandomFactor() {
-        return 0;
+        return new Random().nextInt((MAX_FACTOR-MIN_FACTOR)+1) + MIN_FACTOR;
     }
 }
--- src/test/java/com/hiberus/microservices/training/springmicroservices/service/RandomGeneratorServiceImplTest.java
+++ src/test/java/com/hiberus/microservices/training/springmicroservices/service/RandomGeneratorServiceImplTest.java
@@ -3,6 +3,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,9 +18,9 @@ public class RandomGeneratorServiceImplTest {
     @Test
     public  void generateRandomFactorIsBetweenExpectedLimits(){
         //when a good sample of randomly generated factors is generated
-        List<Integer> randomFactors = IntStream.range(0,1000).map(i -> randomGeneratorServiceImpl.generateRandomFactor()).boxed().toList();
+        List<Integer> randomFactors = IntStream.range(10,50).map(i -> randomGeneratorServiceImpl.generateRandomFactor()).boxed().collect(Collectors.toList());
 
         //then all of them should be between 10 and 50
-        assertThat(randomFactors).containsExactlyElementsOf(IntStream.range(10,50).boxed().toList());
+        assertThat(randomFactors).allMatch(factor -> factor< 50 && factor >= 10);
     }
 }
